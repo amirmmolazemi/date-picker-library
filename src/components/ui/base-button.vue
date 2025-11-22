d9b40bcea623d60613348f45370615e566807483
amirmohammad molazemi <am.molazemi@gmail.com>
2025-11-22 16:51:48 +0330

    feat: add a base button component to create button

---
+++ src/components/ui/base-button.vue
@@ -0,0 +1,20 @@
+<script setup>
+defineProps({
+    text: String,
+})
+</script>
+
+<template>
+    <button>{{ text }}</button>
+</template>
+
+<style lang="scss" scoped>
+button {
+    background-color: $primary-600;
+    height: 48px;
+    padding-inline: 16px;
+    border-radius: 12px;
+    cursor: pointer;
+    color: white;
+}
+</style>
