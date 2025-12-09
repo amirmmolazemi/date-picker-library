3e59f6c0ff363987caf25413d06c9898b3675613
Mr-MKZ <karimierfan74@gmail.com>
2025-12-09 10:25:34 +0330

    chore: Some config improvement to decrease the final bundle file size

--- vite.config.js
+++ vite.config.js
@@ -13,8 +13,9 @@ export default defineConfig({
     preprocessorOptions: {
       scss: {
         additionalData: `
-        @use "@/assets/styles/main" as *;
-      `,
+          @use "@/assets/styles/abstracts/_variables.scss" as *;
+          @use "@/assets/styles/abstracts/_mixins.scss" as *;
+        `,
       },
     },
   },
@@ -26,10 +27,19 @@ export default defineConfig({
       fileName: (format) => `moli-vue-date-picker.${format}.js`,
     },
     rollupOptions: {
-      external: ["vue"],
+      external: [
+        "vue",
+        "vue-i18n",
+        "@vueuse/core",
+        /^date-fns/,
+      ],
       output: {
         globals: {
           vue: "Vue",
+          "vue-i18n": "VueI18n",
+          "@vueuse/core": "VueUse",
+          "date-fns": "DateFns",
+          "date-fns-jalali": "DateFnsJalali",
         },
       },
     },
